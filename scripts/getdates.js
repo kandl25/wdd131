85d8cc186f17aad21cf6cfde7e2d5f4ba2b13633
kandl25 <fundaysrock@yahoo.com>
2024-04-27 23:55:16 -0600

    4/27

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,7 +4,5 @@ const lastModified = document.querySelector("#lastModified")
 const today = new Date();
 const lastMod = document.lastModified;
 
-
 year.innerHTML = `${today.getFullYear()}`;
-
 lastmodified.innerHTML = `Last Modification: ${lastMod}`;
