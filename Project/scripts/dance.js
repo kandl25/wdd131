b23588fd5c689f7e657a2f540617c49db3367bbb
kandl25 <war21039@byui.edu>
2024-06-01 16:33:57 -0600

    6/01

---
+++ Project/scripts/dance.js
@@ -0,0 +1,6 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified")
+const today = new Date();
+const lastMod = document.lastModified;
+year.innerHTML = `${today.getFullYear()}`;
+lastmodified.innerHTML = `Last Modification: ${lastMod}`;
